7043427c2a2f968449d3c29ffdb3e718b25bd3d7
KingJack3012 <3012jaypc2008@gmail.com>
2026-09-25 21:27:32 +0530

    Update Python_Titans.py

--- Python_Titans.py
+++ Python_Titans.py
@@ -22,7 +22,7 @@ st.set_page_config(
 
 GROUP_NO = "Python Titans"
 
-MEMBERS = "Moksh Dhaval Dave <br> | Jay Paraskumar Chaudhary | Anmol Arvindbhai Prajapati "
+MEMBERS = "Moksh Dhaval Dave \n | Jay Paraskumar Chaudhary | Anmol Arvindbhai Prajapati "
 
 ENROLLMENTS = "25012250610045 | 25012250610046 | 25012250610064"
 
